8d46d4424a449931ea08f22e11b62b736a2d0d75
Danil Kozlov <megacokes@gmail.com>
2020-08-11 13:54:49 +0500

    Finish ship-details component

--- src/app/ship-details/ship-details.component.ts
+++ src/app/ship-details/ship-details.component.ts
@@ -1,16 +1,42 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { ShipDetailsGQL } from '../services/spacexGraphql.service';
+import { map, switchMap } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-ship-details',
   templateUrl: './ship-details.component.html',
   styleUrls: ['./ship-details.component.sass'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ShipDetailsComponent implements OnInit {
+export class ShipDetailsComponent {
+  constructor(
+    private readonly route: ActivatedRoute,
+    private shipDetailsService: ShipDetailsGQL
+  ) {}
 
-  constructor() { }
+  shipDetails$ = this.route.paramMap.pipe(
+    map((params) => params.get('id') as string),
+    switchMap((id) => this.shipDetailsService.fetch({ id })),
+    map((res) => res.data.ship)
+  );
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
+
+// import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+// @Component({
+//   selector: 'app-ship-details',
+//   templateUrl: './ship-details.component.html',
+//   styleUrls: ['./ship-details.component.sass'],
+//   changeDetection: ChangeDetectionStrategy.OnPush
+// })
+// export class ShipDetailsComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
